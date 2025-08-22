30798eb518ed9a6476a5dc05cb8d7d053040c156
Kaio <ktorres0312@gmail.com>
2025-08-22 20:46:21 -0300

    Clientes satisfeitos e projetos do counter modificados

--- src/constants/index.js
+++ src/constants/index.js
@@ -11,8 +11,8 @@ export const words =[
 export const counterItems = [
     { value: 4, suffix: "+", label: "Anos de Experiência" },
     { value: 5, suffix: "+", label: "Clientes Satisfeitos" },
-    { value: 15, suffix: "+", label: "Projetos Realizados" },
-    { value: 98, suffix: "%", label: "Taxa de retenção de clientes" },
+    { value: 16, suffix: "+", label: "Projetos Realizados" },
+    { value: 100, suffix: "%", label: "Taxa de retenção de clientes" },
   ];
 export const navLinks =[{
     name: "Trabalhos",
