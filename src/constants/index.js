1d00b53a28dc146cc07a8a65b47e0bef5b59d5cc
Kaio <ktorres0312@gmail.com>
2025-09-20 08:23:00 -0300

    Anos de experiência

--- src/constants/index.js
+++ src/constants/index.js
@@ -9,7 +9,7 @@ export const words =[
 
 
 export const counterItems = [
-    { value: 4, suffix: "+", label: "Anos de Experiência" },
+    { value: 5, suffix: "+", label: "Anos de Experiência" },
     { value: 5, suffix: "+", label: "Clientes Satisfeitos" },
     { value: 16, suffix: "+", label: "Projetos Realizados" },
     { value: 100, suffix: "%", label: "Taxa de retenção de clientes" },
